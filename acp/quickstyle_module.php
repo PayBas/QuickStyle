5bbebb6a53020025175e44ef014c26a59b9ca534
PayBas <contact@paybas.com>
2014-08-14 13:44:34 +0200

    Simplify ACP module object references

--- acp/quickstyle_module.php
+++ acp/quickstyle_module.php
@@ -24,46 +24,36 @@ class quickstyle_module
 {
 	public $u_action;
 
-	protected $user;
-	protected $template;
-	protected $request;
-	protected $config;
-
 	public function main($id, $mode)
 	{
 		global $user, $template, $request, $config;
 
-		$this->user = $user;
-		$this->template = $template;
-		$this->request = $request;
-		$this->config = $config;
-
-		$this->user->add_lang('acp/common');
+		$user->add_lang('acp/common');
 		$this->tpl_name = 'acp_quickstyle';
-		$this->page_title = $this->user->lang('QUICK_STYLE');
+		$this->page_title = $user->lang('QUICK_STYLE');
 
 		$form_key = 'acp_quickstyle';
 		add_form_key($form_key);
 
-		if ($this->request->is_set_post('submit'))
+		if ($request->is_set_post('submit'))
 		{
 			if (!check_form_key($form_key))
 			{
 				trigger_error('FORM_INVALID');
 			}
 
-			$quickstyle_default_loc = $this->request->variable('quickstyle_default_loc', 0);
-			$this->config->set('quickstyle_default_loc', $quickstyle_default_loc);
+			$quickstyle_default_loc = $request->variable('quickstyle_default_loc', 0);
+			$config->set('quickstyle_default_loc', $quickstyle_default_loc);
 
-			$quickstyle_allow_guests = $this->request->variable('quickstyle_allow_guests', 0);
-			$this->config->set('quickstyle_allow_guests', $quickstyle_allow_guests);
+			$quickstyle_allow_guests = $request->variable('quickstyle_allow_guests', 0);
+			$config->set('quickstyle_allow_guests', $quickstyle_allow_guests);
 
-			trigger_error($this->user->lang('CONFIG_UPDATED') . adm_back_link($this->u_action));
+			trigger_error($user->lang('CONFIG_UPDATED') . adm_back_link($this->u_action));
 		}
 
-		$this->template->assign_vars(array(
-			'S_QUICKSTYLE_DEFAULT_LOC'  => isset($this->config['quickstyle_default_loc']) ? $this->config['quickstyle_default_loc'] : true,
-			'S_QUICKSTYLE_ALLOW_GUESTS' => isset($this->config['quickstyle_allow_guests']) ? $this->config['quickstyle_allow_guests'] : true,
+		$template->assign_vars(array(
+			'S_QUICKSTYLE_DEFAULT_LOC'  => isset($config['quickstyle_default_loc']) ? $config['quickstyle_default_loc'] : true,
+			'S_QUICKSTYLE_ALLOW_GUESTS' => isset($config['quickstyle_allow_guests']) ? $config['quickstyle_allow_guests'] : true,
 			'U_ACTION'                  => $this->u_action,
 		));
 	}
